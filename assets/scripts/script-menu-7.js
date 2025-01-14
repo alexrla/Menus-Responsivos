6a473d3144414fb24f058a4ac8a931658a8291ac
Alexrlas <alexrodrigoarruda@gmail.com>
2025-01-14 09:50:45 -0300

    Menu 7

---
+++ assets/scripts/script-menu-7.js
@@ -0,0 +1,9 @@
+const sidebar = document.querySelector(".sidebar")
+
+function showMenu() {
+    sidebar.classList.add("active")
+}
+
+function closeMenu() {
+    sidebar.classList.remove("active")
+}
